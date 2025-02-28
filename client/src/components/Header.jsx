e4520a2d05e0b17b8a5695d763de70355d766b4f
samdev614 <creativeaglemedia@gmail.com>
2025-02-28 13:35:29 +0300

    Profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,9 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Enviprime</span>
-            <span className="text-slate-700">Real Estate</span>
+            <span className="text-slate-500">Enviprime </span> 
+          
+            <span className="mx-2 text-slate-700"> Real Estate</span> 
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
